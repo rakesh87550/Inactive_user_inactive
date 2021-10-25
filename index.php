2e61298aa3fea53b0b7d5849a4dcc4c00973c000
Rakesh Saha <rs2382@cse.jgec.ac.in>
2021-10-25 14:06:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,22 @@
 <?php
-session_start();
-$con=mysqli_connect('localhost','root','','inactive_user_logout');
-$msg="";
-if(isset($_POST['submit'])){
-	$username=mysqli_real_escape_string($con,$_POST['username']);
-	$password=mysqli_real_escape_string($con,$_POST['password']);
-	
-	$res=mysqli_query($con,"select * from user where username='$username' and password='$password'");
-	if(mysqli_num_rows($res)>0){
-		$_SESSION['IS_LOGIN']='yes';
-		header('location:dashboard.php');
-		die();
-	}else{
-		$msg="Please enter valid login details";
-	}
-}
+   session_start();
+   // database connect
+   $con=mysqli_connect('localhost','root','','inactive_user_logout');
+   $msg="";
+   // check login details
+   if(isset($_POST['submit'])){
+      $username=mysqli_real_escape_string($con,$_POST['username']);
+      $password=mysqli_real_escape_string($con,$_POST['password']);
+      
+      $res=mysqli_query($con,"select * from user where username='$username' and password='$password'");
+      if(mysqli_num_rows($res)>0){
+         $_SESSION['IS_LOGIN']='yes';
+         header('location:dashboard.php');
+         die();
+      }else{
+         $msg="Please enter valid login details";
+      }
+   }
 ?>
 
 <!DOCTYPE html>
@@ -25,6 +27,7 @@ if(isset($_POST['submit'])){
       <title>Login Form</title>
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <link href="bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+
       <style type="text/css">
          body {margin: 0;padding: 0;background-color: #17a2b8;height: 100vh;}
          #login .container #login-row #login-column #login-box {margin-top: 60px;max-width: 600px;height: 320px;border: 1px solid #9C9C9C;background-color: #EAEAEA;}
@@ -43,6 +46,7 @@ if(isset($_POST['submit'])){
                <div id="login-row" class="row justify-content-center align-items-center">
                   <div id="login-column" class="col-md-6">
                      <div id="login-box" class="col-md-12">
+                        <!-- login form -->
                         <form id="login-form" class="form" method="post">
                            <div class="form-group">
                               <label for="username" class="font-weight-bold">Username:</label><br>
