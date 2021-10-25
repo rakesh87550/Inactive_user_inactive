00a4129ea01e56d23789a91f5d4aa9956230c0e4
Rakesh Saha <rs2382@cse.jgec.ac.in>
2021-10-25 14:05:27 +0530

    Update dashboard1.php

--- dashboard1.php
+++ dashboard1.php
@@ -1,27 +1,28 @@
 <?php
-require("user_auth.php");
-echo "Hello<br>";
+	// include logout count page
+	require("user_auth.php");
+	echo "Hello<br>";
 ?>
 <a href="dashboard.php">Dashboard</a><br/>
 <a href="logout.php">Logout</a>
 
 <script src="jquery-3.4.1.min.js"></script>
 <script>
-setInterval(function(){
-	check_user();
-},2000);
-
-function check_user(){
-	jQuery.ajax({
-		url:'user_auth.php',
-		type:'post',
-		data:'type=ajax',
-		success:function(result){
-			if(result=='logout'){
-				window.location.href='logout.php';
+	// function to hit page every 2sec
+	setInterval(function(){
+		check_user();
+	},2000);
+	// function to logout
+	function check_user(){
+		jQuery.ajax({
+			url:'user_auth.php',
+			type:'post',
+			data:'type=ajax',
+			success:function(result){
+				if(result=='logout'){
+					window.location.href='logout.php';
+				}
 			}
-		}
-		
-	});
-}
+		});
+	}
 </script>
